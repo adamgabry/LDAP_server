ca52f6d65c1fb235f7f261e11db20a172a1e6c77
adamgabry <adam.gabrys@seznam.cz>
2023-10-25 22:22:14 +0200

    added library

--- ldap_functions.h
+++ ldap_functions.h
@@ -1,10 +1,10 @@
 #ifndef LDAP_FUNCTIONS_H
+#define LDAP_FUNCTIONS_H
 
-    #define LDAP_FUNCTIONS_H
     #define DEBUG 1
     #define PORT 389
-    pthread_t tid;
 
+    void sendBindResponse(int client_socket);
     void handleBindRequest(int client_socket);
 
 #endif
