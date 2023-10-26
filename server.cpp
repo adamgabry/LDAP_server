3d0939ed4b647b28c4ba0c0be0a5c4ffdb7ad059
adamgabry <adam.gabrys@seznam.cz>
2023-10-26 19:17:46 +0200

    added parsing file

--- server.cpp
+++ server.cpp
@@ -1,7 +1,12 @@
 // Standard C++ libraries
 #include <iostream>
+#include <fstream> // Include the necessary header for file operations
 #include <cstdlib>
 #include <cstring>
+#include <sstream>
+#include <string.h>
+#include <vector>
+#include <set>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,7 +17,6 @@
 #include "ldap_functions.h"
 #include "server.h"
 
-#define ERR 1
 
 void* client_handler(void* arg) {
     int client_socket = *((int*)arg);
@@ -23,40 +27,83 @@ void* client_handler(void* arg) {
     return NULL;
 }
 
-server::server(int port) {
+server::server(int port) 
+{
 
     // Create a socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+
+    if (server_socket == -1) 
+    {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
-    // Configure the server address structure
+    //clear memory
     memset(&server_addr, 0, sizeof(server_addr));
+
+    // Configure the server address structure
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
     server_addr.sin_port = htons(port); 
 
     // Bind the socket to the server address
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) 
+    {
         perror("Error binding socket");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
 
     // Start listening for incoming connections
-    if (listen(server_socket, 5) == -1) {
+    if (listen(server_socket, 5) == -1) 
+    {
         perror("Error listening");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
 }
-void server::connect_clients(){
-    while (1) {
+
+
+// Function to parse the database
+void server::parse_database(string input_file) 
+{
+    ifstream infile(input_file);
+
+    if(!infile)
+    {
+        cerr << "Opening the file failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+    while (getline(infile, line))
+    {
+        istringstream iss(line);
+        if (getline(iss, cn, ';') && getline(iss, uid, ';') && getline(iss, email)) 
+        {
+            data.push_back(cn);
+            data.push_back(uid);
+            data.push_back(email);
+            database.emplace(data);
+        }
+        else
+        {
+            cerr << "Failed to parse the line: " << line << endl;
+        }
+        
+        for (const string& item : data) {
+            cout << item << endl;
+    }
+    }
+}
+
+void server::connect_clients()
+{
+    while (1) 
+    {
         // Accept incoming connections
         client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-        if (client_socket == -1) {
+        if (client_socket == -1) 
+        {
             perror("Error accepting connection");
             continue;
         }
@@ -71,24 +118,46 @@ void server::connect_clients(){
 
         // Start handling the communication with the client
         // If unsuccessful, close the client socket and free the allocated memory
-        // Here, client_handler is the function that calls the handleBindRequest function
-        pthread_t tid;
-        if (pthread_create(&tid, NULL, client_handler, client_socket_ptr) != 0) {
+        // client_handler is the function that calls the handleBindRequest function
+        if (pthread_create(&tid, NULL, client_handler, client_socket_ptr) != 0) 
+        {
             perror("Error creating thread");
             close(client_socket);
             free(client_socket_ptr);
         }
     }
-close(server_socket);
+    close(server_socket);
 }
 
-int main() {
 
-    server ser(PORT);
+int main(int argc, char *argv[]) 
+{
+    int opt = 0;
+    string file_name = "";
+    int port = 389;
+    while ((opt = getopt(argc, argv, "p:f:")) != -1) 
+    {
+        switch (opt) 
+        {
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'f':
+                file_name = optarg;
+                break;
+            default:
+                cerr << "Argument parse error" << endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    server ldap_server(PORT);
 
     std::cout << "LDAP server is listening on port " << PORT << "..." << std::endl;
     
-    ser.connect_clients();
+    ldap_server.parse_database(file_name);
+
+    ldap_server.connect_clients();
 
     return 0;
 }
--- server.h
+++ server.h
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstdlib>
-#include <cstring>
+#include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <pthread.h>
 
+#define ERR 1
+
+using namespace std; 
+
 /*
-* handles more clients at the time.
+* handles more clients at the time
+* and calls handleBindRequest function  
 */
 void* client_handler(void* arg);
 
@@ -16,13 +21,39 @@ void* client_handler(void* arg);
 class server
 {
 private:
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int* client_socket_ptr;
-public:
-    server(int port);
 
-    void connect_clients();
+    /// vars for communication
+    ///
+    int server_socket, 
+        client_socket;
+    struct sockaddr_in  server_addr,
+                        client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    pthread_t tid;
+    int* client_socket_ptr;
+    ///
     
+    /// vars for file parsing
+    ///
+    vector<string> data;
+    string  line,
+            uid,
+            cn,
+            email;
+    using StringVectorSet = set<vector<string>>; // Define a type alias for convenience
+    StringVectorSet database;
+
+public:
+    /*
+    *@brief here is established server itself
+    */
+    server(int port);
+    /*
+    *@brief for creating threads and establishing connection with clients
+    */
+    void connect_clients();
+    /*
+    *parsing data from csv file
+    */
+    void parse_database(string input_file);
 };
